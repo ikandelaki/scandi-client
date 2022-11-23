af1e1669303e25af2b897e549bc0ae4761e15c82
kandelseik <iraklikandelaki2@gmail.com>
2022-11-23 12:50:26 +0400

    Edit proxy and server base URL

--- src/api/queryFetch.js
+++ src/api/queryFetch.js
@@ -1,5 +1,5 @@
 const queryFetch = (query, variables) => {
-  return fetch("http://localhost:4000/", {
+  return fetch("https://scandi-project-server.herokuapp.com/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
